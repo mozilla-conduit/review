e76fe505d8cbd8f692a4cd76b19e5166efb52945
Piotr Zalewa <zaloon@gmail.com>
2018-11-08 12:56:47 +0100

    Bug 1504934 Raise if any commit is not a child of the first one r=glob,mars
    Summary:
    Running `moz-phab submit feature` on a repository with a DAG as
    below would break the history
    
    * (master) ccc
    | * (feature) bbb
    | * aaa
    |/
    ...
    
    This patch is checking if any of the commits is not a child of the first
    one and raises an Error if so.
    
    Reviewers: glob, mars
    
    Reviewed By: mars
    
    Bug #: 1504934
    
    Differential Revision: https://phabricator.services.mozilla.com/D11312

--- tests/test_git.py
+++ tests/test_git.py
@@ -75,3 +75,76 @@ class TestGit(unittest.TestCase):
         git.args = Args(upstream=["upstream"])
         first()
         m_git_cherry.assert_called_with(["cherry", "--abbrev=12", "upstream"], [])
+
+    def test_get_dirsect_children(
+        self,
+        m_repository_phab_url,
+        m_which,
+        m_os_path,
+        m_config,
+        m_git_get_current_head,
+        m_git_git_out,
+    ):
+        m_os_path.join = os.path.join
+        m_os_path.exists.return_value = True
+        m_which.return_value = True
+        m_os_path.isfile.return_value = False
+        m_git_get_current_head.return_value = "branch"
+        m_git_git_out.return_value = git_config
+        git = review.Git("x")
+
+        get_children = git._get_direct_children
+        rev_list = ["aaa bbb ccc", "bbb", "ccc ddd"]
+
+        self.assertEqual(["bbb", "ccc"], get_children("aaa", rev_list))
+        self.assertEqual([], get_children("bbb", rev_list))
+        self.assertEqual(["ddd"], get_children("ccc", rev_list))
+        self.assertEqual([], get_children("xxx", rev_list))
+
+
+    def test_is_child(
+        self,
+        m_repository_phab_url,
+        m_which,
+        m_os_path,
+        m_config,
+        m_git_get_current_head,
+        m_git_git_out,
+    ):
+        m_os_path.join = os.path.join
+        m_os_path.exists.return_value = True
+        m_which.return_value = True
+        m_os_path.isfile.return_value = False
+        m_git_get_current_head.return_value = "branch"
+        m_git_git_out.return_value = git_config
+        git = review.Git("x")
+
+        is_child = git._is_child
+        # * ccc
+        # * bbb
+        # * aaa
+        nodes = ["ccc", "bbb ccc", "aaa bbb"]
+        self.assertTrue(is_child("aaa", "bbb", nodes))
+        self.assertTrue(is_child("aaa", "ccc", nodes))
+        self.assertTrue(is_child("bbb", "ccc", nodes))
+        self.assertFalse(is_child("bbb", "aaa", nodes))
+        self.assertFalse(is_child("aaa", "aaa", nodes))
+        self.assertFalse(is_child("bbb", "bbb", nodes))
+        self.assertFalse(is_child("ccc", "ccc", nodes))
+
+        # * ddd
+        # | * ccc
+        # | | * eee
+        # | |/
+        # | * bbb
+        # |/
+        # * aaa
+        nodes = ["ddd", "ccc", "eee", "bbb ccc eee", "aaa bbb ddd"]
+        self.assertTrue(is_child("aaa", "bbb", nodes))
+        self.assertTrue(is_child("aaa", "ccc", nodes))
+        self.assertTrue(is_child("aaa", "ddd", nodes))
+        self.assertTrue(is_child("aaa", "eee", nodes))
+        self.assertTrue(is_child("bbb", "ccc", nodes))
+        self.assertTrue(is_child("bbb", "eee", nodes))
+        self.assertFalse(is_child("bbb", "ddd", nodes))
+        self.assertFalse(is_child("ccc", "ddd", nodes))
