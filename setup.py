318fdad4337462e27b46a70dc881d1673fcf4a2b
Piotr Zalewa <zaloon@gmail.com>
2019-10-21 12:45:40 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="MozPhab",
-    version="0.1.52",
+    version="0.1.53",
     author="Mozilla",
     author_email="conduit-team@mozilla.com",
     scripts=["moz-phab"],
