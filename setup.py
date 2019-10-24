9dbf6318fa139cf5e50785d9f3d823428de1d9e3
Piotr Zalewa <zaloon@gmail.com>
2019-10-24 15:30:41 +0200

    Bug 1591015 - Temporarily switch off --no-arc on Windows. r=glob
    Differential Revision: https://phabricator.services.mozilla.com/D50472

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="MozPhab",
-    version="0.1.55",
+    version="0.1.56",
     author="Mozilla",
     author_email="conduit-team@mozilla.com",
     scripts=["moz-phab"],
