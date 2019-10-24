cf4798ca34d48c1ec87248c995242a23877d071a
Piotr Zalewa <zaloon@gmail.com>
2019-10-24 11:37:07 +0200

    ++ version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="MozPhab",
-    version="0.1.54",
+    version="0.1.55",
     author="Mozilla",
     author_email="conduit-team@mozilla.com",
     scripts=["moz-phab"],
