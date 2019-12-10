f0f7483c077fa5c5c44583b0e7be4933c24f918e
Piotr Zalewa <zaloon@gmail.com>
2019-12-10 21:20:44 +0100

    release 0.1.66

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="MozPhab",
-    version="0.1.65",
+    version="0.1.66",
     author="Mozilla",
     author_email="conduit-team@mozilla.com",
     scripts=["moz-phab"],
