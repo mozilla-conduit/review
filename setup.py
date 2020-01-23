f263be859acf51e67b9d69ad65bf25a512c85c38
Piotr Zalewa <zaloon@gmail.com>
2020-01-23 13:02:29 +0100

    release 0.1.74

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="MozPhab",
-    version="0.1.73",
+    version="0.1.74",
     author="Mozilla",
     author_email="conduit-team@mozilla.com",
     packages=["mozphab"],
