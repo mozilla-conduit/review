783f2f79f7b520bc4616ecb9a90512007c58c8ff
Piotr Zalewa <zaloon@gmail.com>
2020-03-11 17:05:02 +0100

    release 0.1.80

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if "develop" in sys.argv:
 
 setup(
     name="MozPhab",
-    version="0.1.79",
+    version="0.1.80",
     author="Mozilla",
     author_email="conduit-team@mozilla.com",
     packages=["mozphab"],
