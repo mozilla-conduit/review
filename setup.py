55a75769dc81f6908c3923c6ca5ed618d7621d2c
Byron Jones <byron@glob.com.au>
2020-04-16 14:05:42 +0800

    release 0.1.85

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if "develop" in sys.argv:
 
 setup(
     name="MozPhab",
-    version="0.1.84",
+    version="0.1.85",
     author="Mozilla",
     author_email="conduit-team@mozilla.com",
     packages=["mozphab", "mozphab/commands"],
