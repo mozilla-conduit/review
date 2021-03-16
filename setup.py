456b359cf5fc2edc481f0585c6fd73da4dee6365
Zeid Zabaneh <zeid@mozilla.com>
2021-03-16 12:04:17 -0400

    Bug 1696346 - Explicitly set `zip_safe` to `False` r=sheehan,dkl,glob
    Differential Revision: https://phabricator.services.mozilla.com/D108628

--- setup.py
+++ setup.py
@@ -7,22 +7,11 @@ if "develop" in sys.argv:
     entry_points["console_scripts"].append("moz-phab-dev = mozphab.mozphab:run_dev")
 
 setup(
-    name="MozPhab",
-    version="0.1.95",
     author="Mozilla",
     author_email="conduit-team@mozilla.com",
-    packages=["mozphab", "mozphab/commands"],
-    entry_points=entry_points,
-    url="https://github.com/mozilla-conduit/review",
-    license="Mozilla Public License 2.0",
     description="Phabricator review submission/management tool.",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    # Note: Please change the `updater.py::check_for_updates` method if the format
-    # would be different than >=X.Y
-    python_requires=">=3.6",
+    entry_points=entry_points,
     include_package_data=True,
-    package_data={"mozphab": ["metrics.yaml", "pings.yaml"]},
     install_requires=[
         "distro",
         "glean-sdk>=33.10.1",
@@ -30,4 +19,16 @@ setup(
         "sentry-sdk>=0.14.3",
         "setuptools",
     ],
+    license="Mozilla Public License 2.0",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    name="MozPhab",
+    package_data={"mozphab": ["metrics.yaml", "pings.yaml"]},
+    packages=["mozphab", "mozphab/commands"],
+    # Note: Please change the `updater.py::check_for_updates` method if the format
+    # would be different than >=X.Y
+    python_requires=">=3.6",
+    url="https://github.com/mozilla-conduit/review",
+    version="0.1.95",
+    zip_safe=False,
 )
