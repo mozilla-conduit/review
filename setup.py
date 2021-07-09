dbc5c1982bbeb3fe23ff6fe8456d7b99282c0aaa
Zeid Zabaneh <zeid@mozilla.com>
2021-07-09 15:05:03 -0400

    release candidate 1.0.0rc1

--- setup.py
+++ setup.py
@@ -29,6 +29,6 @@ setup(
     # would be different than >=X.Y
     python_requires=">=3.6",
     url="https://github.com/mozilla-conduit/review",
-    version="0.1.99",
+    version="1.0.0rc1",
     zip_safe=False,
 )
