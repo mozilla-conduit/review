9b9dbfaa63b053b152e71267a5cd72694601c2b2
Connor Sheehan <cosheehan@mozilla.com>
2022-03-15 12:40:37 -0400

    meta: bump version to `1.1.0rc0`

--- setup.py
+++ setup.py
@@ -29,6 +29,6 @@ setup(
     # would be different than >=X.Y
     python_requires=">=3.6",
     url="https://github.com/mozilla-conduit/review",
-    version="1.0.0",
+    version="1.1.0rc1",
     zip_safe=False,
 )
