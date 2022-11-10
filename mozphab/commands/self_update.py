21df3b842405ef58fe1f384000d2e016c62c820b
Connor Sheehan <cosheehan@mozilla.com>
2022-11-10 11:10:50 -0500

    self-update: log a message to the screen when no update is available (Bug 1798751) r=zeid
    When an update is available we now have some logging. However when
    no update is available, there is still no command output. Add
    a log message that states there is nothing for `moz-phab` to do.
    
    Differential Revision: https://phabricator.services.mozilla.com/D161810

--- mozphab/commands/self_update.py
+++ mozphab/commands/self_update.py
@@ -1,7 +1,10 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
+
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from mozphab.environment import MOZPHAB_VERSION
+from mozphab.logger import logger
 from mozphab.spinner import wait_message
 from mozphab.updater import check_for_updates, self_upgrade
 
@@ -9,7 +12,11 @@ from mozphab.updater import check_for_updates, self_upgrade
 def self_update(_):
     """`self-update` command, updates this package"""
     new_version = check_for_updates(force_check=True)
+
     if not new_version:
+        logger.info(
+            f"You are running the latest version of `moz-phab`, {MOZPHAB_VERSION}."
+        )
         return
 
     with wait_message(f"Upgrading to version {new_version}"):
