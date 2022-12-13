cf93be672aa1c99f3ee15f9f6244791e7f7685c8
Connor Sheehan <cosheehan@mozilla.com>
2022-12-13 15:33:15 -0500

    meta: add license headers to files where it is missing (Bug 1805530) r=glob
    Differential Revision: https://phabricator.services.mozilla.com/D164598

--- update_requirements_linux.sh
+++ update_requirements_linux.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 
 docker-compose -f dev/docker-compose.yml run generate-python3.7-requirements
 docker-compose -f dev/docker-compose.yml run generate-python3.8-requirements
