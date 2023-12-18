d92869a5a7e96b6808deb73e2ea2733ea384da13
Connor Sheehan <cosheehan@mozilla.com>
2023-12-18 15:42:07 -0500

    typing: add type hints to `simplecache.py` (Bug 1870284) r=zeid
    Differential Revision: https://phabricator.services.mozilla.com/D196782

--- mozphab/simplecache.py
+++ mozphab/simplecache.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from typing import Any
+
 
 class SimpleCache:
     """Simple key/value store with all lowercase keys."""
@@ -9,16 +11,16 @@ class SimpleCache:
     def __init__(self):
         self._cache = dict()
 
-    def __contains__(self, key):
+    def __contains__(self, key: str) -> bool:
         return key.lower() in self._cache
 
-    def get(self, key):
+    def get(self, key: str) -> Any:
         return self._cache.get(key.lower())
 
-    def set(self, key, value):
+    def set(self, key: str, value: Any):
         self._cache[key.lower()] = value
 
-    def delete(self, key):
+    def delete(self, key: str):
         if key in self:
             del self._cache[key.lower()]
 
